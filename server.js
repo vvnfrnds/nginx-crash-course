f7bb27139dc47f191749b9c50d545be84bb2ce92
Nana Janashia <nn_janashia@yahoo.com>
2024-10-24 07:00:30 +0000

    Update file server.js

--- server.js
+++ server.js
@@ -3,13 +3,15 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
+const appName = process.env.APP_NAME
+
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.use('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
-    console.log(`Request served by ${process.env.APP_NAME}`);
+    console.log(`Request served by ${appName}`);
 });
 
 app.listen(port, () => {
-    console.log(`${process.env.APP_NAME} is listening on port ${port}`);
+    console.log(`${appName} is listening on port ${port}`);
 });
